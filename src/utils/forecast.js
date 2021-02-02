54147fb9944fba5c88ec5642f9dbab417d6d8dc2
LAPTOP-2F9TUJ9T\philip <prechtacek@mogiletech.com>
2021-02-02 09:16:09 -0500

    Modified weather message

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 forecast = (latitude, longitude, callback) => {
-  const url = 'http://api.weatherstack.com/current?access_key=cd9181eaaebf8b062b96f625ba018f0b&query=' + latitude + ',' + longitude + '&units=f'
+  const url = 'http://api.weatherstack.com/current?access_key=cd9181eaaebf8b062b96f625ba018f0b&query=' + latitude + ',' + longitude
   
   request({url, json: true}, (error, {body}) =>{
     if(error){
@@ -9,7 +9,7 @@ forecast = (latitude, longitude, callback) => {
     }else if(body.error){
       callback('Unable to find location. Please try again.', undefined)
     }else{
-      callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. There is currently ${body.current.precip}% chance of rain.`)
+      callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out with winds of ${body.current.wind_speed}km/h.`)
     }
   })
 }
